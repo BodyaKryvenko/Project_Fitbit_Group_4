881ae9fb781748de4f3a7fcac6e4ce541fef110e
BodyaKryvenko <bodya.kryvenko@gmail.com>
2025-03-23 06:15:11 +0100

    Bohdan Kryvenko - Made load data functions.

--- part5.py
+++ part5.py
@@ -1,7 +1,49 @@
 import streamlit as st
+import pandas as pd
+import sqlite3
 
 st.set_page_config(
     page_title="Fitbit User Analysis Dashboard",
     layout="wide",
     initial_sidebar_state="expanded"
 )
+
+
+def load_activity_data(file_path):
+    activity_df = pd.read_csv(file_path)
+    return activity_df
+
+
+def load_database_data(file_path):
+    conn = sqlite3.connect(file_path)
+
+    hourly_steps = pd.read_sql_query("SELECT * FROM hourly_steps", conn)
+    hourly_calories = pd.read_sql_query("SELECT * FROM hourly_calories", conn)
+    hourly_intensity = pd.read_sql_query("SELECT * FROM hourly_intensity", conn)
+    daily_activity = pd.read_sql_query("SELECT * FROM daily_activity", conn)
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='minute_sleep'")
+    if cursor.fetchone():
+        sleep_data = pd.read_sql_query("SELECT * FROM minute_sleep", conn)
+    else:
+        sleep_data = pd.DataFrame()
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='weight_log'")
+    if cursor.fetchone():
+        weight_data = pd.read_sql_query("SELECT * FROM weight_log", conn)
+    else:
+        weight_data = pd.DataFrame()
+
+    conn.close()
+
+    data = {
+        'hourly_steps': hourly_steps,
+        'hourly_calories': hourly_calories,
+        'hourly_intensity': hourly_intensity,
+        'daily_activity': daily_activity,
+        'sleep_data': sleep_data,
+        'weight_data': weight_data
+    }
+
+    return data
